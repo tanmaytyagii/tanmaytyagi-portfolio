e838e9938cf994ce7585bf8c0772213fa65f59bf
Tanmay Tyagi <168903313+tanmaytyagii@users.noreply.github.com>
2026-09-13 10:01:01 +0530

    fix: migrate gsap trial plugins to standard gsap

--- .vite/deps/react-dom_client.js
+++
@@ -1,7 +0,0 @@
-import {
-  require_client
-} from "./chunk-JX5S4TE2.js";
-import "./chunk-TWJRYSII.js";
-import "./chunk-DC5AMYBS.js";
-export default require_client();
-//# sourceMappingURL=react-dom_client.js.map
